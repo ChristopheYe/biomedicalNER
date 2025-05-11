e6fda6ba30f0fbd0ab24440797efb61a33c2bb9a
Ye_Christophe_cye73 <cye73@MITCH-LIN.AD.GATECH.EDU>
2025-05-11 19:47:45 -0400

    Add data

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name="biomedical_NER",
+    description="LLM for biomedical named entity recognition",
+    # long_description=long_description,
+    url="",
+    author="Anonymous",
+    author_email="",
+    keywords=[
+        "biomedical",
+        "noisy dataset",
+        "named entity recognition",
+    ],
+    packages=find_packages(),
+    python_requires=">= 3.9",
+    install_requires=[
+        "tqdm",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "ujson",
+        "torch",
+        "transformers",
+        "faiss-gpu",
+        "sentence_transformers",
+        "vllm",
+        "openai",
+        "wandb",
+        "inflect",
+        "evaluate",
+        "datasets",
+        "thefuzz",
+        "spacy",
+        "peft",
+        "seqeval",
+        "bioc",
+    ],
+)
